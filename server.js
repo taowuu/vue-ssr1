74603de01a074ada27772c073993dcabd2206c39
tao <2098613620@qq.com>
2022-09-10 19:02:17 +0800

    1

---
+++ server.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const Vue = require('vue');
+const Koa = require('koa');
+const KoaRouter = require('koa-router');
+const serve = require('koa-static');
+const VueServerRenderer = require('vue-server-renderer');
+
+const serverBundle = fs.readFileSync('./dist/server.bundle.js', 'utf-8');
+const template = fs.readFileSync('./dist/index.ssr.html', 'utf-8');
+
+const renderer = VueServerRenderer.createBundleRenderer(serverBundle, {
+  template
+})
+
+const app = new Koa();
+const router = new KoaRouter();
+
+router.get('(.*)', async ctx => {
+  ctx.body = await renderer.renderToString();
+})
+
+app.use(serve(path.resolve(__dirname, 'dist')))
+
+app.use(router.routes())
+
+app.listen(8080, () => {
+  console.log('服务器运行在8080端口...')
+});
+
+
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div class="demo" @click="onClick">
+      一段内容
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClick() {
+      console.log('click!!!!!');
+    }
+  }
+}
+</script>
+
+<style>
+.demo {
+  width: 300px;
+  height: 300px;
+  background-color: orange;
+}
+</style>
